8a549facf8263c3e830cc568032d2b2c2f4e69f3
makom <mako.mail.g@gmail.com>
2022-08-24 16:22:08 +0900

    Rails7より、APIモードのようにセッションを使用しない場合にセッションへのアクセスがあるとエラーを発生させる機構が備わったことによるエラーの解消
    https://blog.furu07yu.com/entry/rails7-devise-token-auth

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
-    if current_user
-      render json: { status: 200, current_user: current_user }
+    if current_api_v1_user
+      render json: { status: 200, current_user: current_api_v1_user }
     else
       render json: { status: 500, message: "ユーザーが存在しません" }
     end
